ca64c7fa2de276fbaf8e550605f101b575e4a8c9
Caio Alves <kaio_rx@hotmail.com>
2023-03-05 02:15:11 -0300

    feat: add loader

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import {LoadingPlugin} from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
 
 import '../node_modules/bulma/css/bulma.css'
 import './assets/main.css'
@@ -9,5 +11,6 @@ import './assets/main.css'
 const app = createApp(App)
 
 app.use(router)
+app.use(LoadingPlugin);
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,42 @@ import ThePrincipalCard from '../components/ThePrincipalCard.vue';
 import TheCardsExpirience from '../components/TheCardsExpirience.vue';
 import TheCardsFormation from '../components/TheCardsFormation.vue';
 import TheContainer from '../components/TheContainer.vue';
+import TheFooter from '../components/TheFooter.vue';
+
+
+import {ref, inject, onMounted} from 'vue'
+import {useLoading} from 'vue-loading-overlay'
+
+const isLoading = ref(true)
+
+const $loading = useLoading({
+        // options
+});
+
+const fullPage = ref(true)
+
+const loading = () => {
+    const loader = $loading.show({
+      opacity: 0.7,
+    });
+    setTimeout(() => {
+        loader.hide()
+    }, 1000)
+}
+
+onMounted(() => {
+  loading()
+})
+
 </script>
 
 <template>
-  <section class="section">
+  <section class="section vl-parent">
+
+    <loading v-model:active="isLoading"
+                 :can-cancel="false"
+                 :is-full-page="fullPage"/>
+
 
     <TheContainer>
 
@@ -27,4 +59,5 @@ import TheContainer from '../components/TheContainer.vue';
     </TheContainer>
 
   </section>
+  <TheFooter/>
 </template>
